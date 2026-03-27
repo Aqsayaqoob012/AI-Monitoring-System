4bfa73d1e96c557c29a832ba5c457eff302ceb0b
ayeshasad150 <ayeshasad150@gmail.com>
2026-03-28 00:09:02 +0500

    change

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ model = YOLO("best.pt")
 PHONE_CLASS = 1
 PERSON_CLASS = 3
 
+
 # ── Global State ─────────────────────────────────────────────
 cap              = cv2.VideoCapture(0)
 person_state     = None
@@ -130,16 +131,17 @@ def process_frame(frame, current, frame_num=0, is_video=False):
         x1, y1, x2, y2 = boxes.xyxy[i]
         area = (x2 - x1) * (y2 - y1)
 
-    # 👤 PERSON
-        if cls == PERSON_CLASS and conf > 0.5:
-            if area > 100000:   # ignore small false persons
+        #person 
+        if cls == PERSON_CLASS and conf > 0.4:
+            if area > 80000:   # ignore small false persons
                 num_persons += 1
     
-    # 📱 PHONE (SUPER STRICT 🔥)
-        elif cls == PHONE_CLASS and conf > 0.7:
+        # phone(strict filter)
+        elif cls == PHONE_CLASS and conf > 0.6:
+            # center position check
             box_center_x = int((x1 + x2) / 2)
-    # FINAL FILTER
-            if area < 30000 and abs(box_center_x - center_x) < 150:   # 👈 IMPORTANT (tune kar sakti ho)
+
+            if area < 50000:   # 👈 IMPORTANT (tune kar sakti ho)
                phone_detected = True
 
     
